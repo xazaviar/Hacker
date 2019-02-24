e4bede2bafd444e508a122c0438de8d45b15884b
Joseph Ryan <xazaviar@gmail.com>
2019-02-24 13:07:01 -0500

    Base Terminal Designed/Built

--- public/js/game/Drawing.js
+++ public/js/game/Drawing.js
@@ -1,17 +1,94 @@
+//**********************************************************************
+//INIT
+//**********************************************************************
 function Drawing(context) {
     this.context = context;
     this.scale = 3;
+
+    this.loadImages();
+
+    //Blinking cursor
+    //this.cursorSymbol = "█"; //https://coolsymbol.com/square-rectangle-symbols.html
+    this.cursorVis = false;
+    var cont = this;
+    setInterval(function(){cont.cursorVis=!cont.cursorVis;},500);
 }
 
 Drawing.create = function(context){
     return new Drawing(context);
 }
 
+
+//**********************************************************************
+//DRAWING
+//**********************************************************************
+Drawing.prototype.background = function(status){
+    var canvas = this.context.canvas;
+
+    //Draw cyber backing
+    this.context.beginPath();
+    this.context.drawImage(this.backImg,0,0,canvas.width,canvas.height);
+
+    //Draw main background cover (visibility)
+    this.context.fillStyle = 'rgba(0,0,25,.98)';
+    this.context.fillRect(0, 0, canvas.width, canvas.height);
+    this.context.fill();
+}
+
+Drawing.prototype.status = function(status){
+    
+}
+
+Drawing.prototype.terminal = function(status, log, command, cursorIndex){
+    var scale = 1920/this.context.canvas.width;
+    var offset = 15/scale, viewWid = this.context.canvas.width * .70;
+    var color = 'rgb(0,255,255,.95)';
+    var fontSize = (14/scale), fontWid = fontSize*.6;
+    this.context.font = ""+fontSize+"px Courier New";
+    this.context.fillStyle = color;
+    this.context.strokeStyle = color;
+
+    //Draw Border
+    this.context.strokeRect(offset, offset, viewWid, canvas.height - offset*2);
+    this.context.fill();
+
+    //Draw entry text
+    this.context.fillText("> "+command /*+ (this.cursorVis?this.cursorSymbol:"")*/, offset*2, canvas.height - offset*2); 
+
+    //Draw cursor
+    if(this.cursorVis)
+        this.context.fillRect(offset*2+(cursorIndex+2)*fontWid,canvas.height - offset*2-fontSize*.8,fontWid/2,fontSize);
+
+    //Draw terminal text
+    var count = 1, max_len = 156, max_line = 74;
+    for(var l = log.length-1; l > -1 && count < max_line; l--){
+        if(log[l].length > max_len){
+            var split = log[l];
+            for(var split = parseInt(log[l].length/max_len); split > -1; split--){
+                this.context.fillText(log[l].substring(split*max_len,Math.min(log[l].length-1,split*max_len+max_len)),offset*2, canvas.height - offset*2 - count*fontSize);
+                count++;
+            }
+        }
+        else{
+            this.context.fillText(log[l],offset*2, canvas.height - offset*2 - count*fontSize);
+            count++;
+        }
+    }
+
+}
+
+Drawing.prototype.chat = function(status, log){
+    
+}
+
+
+
+//**********************************************************************
+//UTIL
+//**********************************************************************
 Drawing.prototype.clear = function() {
     var canvas = this.context.canvas;
     this.context.clearRect(0, 0, canvas.width, canvas.height);
-    this.context.fillStyle = '#00F';
-    this.context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 Drawing.prototype.resize = function(){
@@ -36,4 +113,16 @@ Drawing.prototype.resize = function(){
 
     $("#canvas").attr("width",newWidth+"px");
     $("#canvas").attr("height",newHeight+"px");
+
+
+    this.scale = newWidth;
+}
+
+Drawing.prototype.setScale = function(s) {
+    this.scale = s;
+}
+
+Drawing.prototype.loadImages = function(){
+    this.backImg = new Image;
+    this.backImg.src = "/images/backdrop.png";
 }
--- public/js/game/Game.js
+++ public/js/game/Game.js
@@ -1,4 +1,4 @@
-const FPS = 60;
+const FPS = 20;
 
 function Game(socket, drawing){
     this.socket = socket;
@@ -6,7 +6,63 @@ function Game(socket, drawing){
 
     this.user = {id:"", auth:""};
 
-    console.log(this);
+    //Terminal Data
+    this.status = "clear";
+    this.validCommands = ["clear", "sys",""];
+    this.command = "";
+    this.prevEntries = [];
+    this.prevEntIndex = this.prevEntries.length;
+    this.terminal = [];
+    this.cursorIndex = 0;
+
+
+    //Keyboard listener
+    var context = this;
+    $(document).on('keyup keydown', function(e){this.shifted = e.shiftKey} );
+    window.addEventListener('keydown',function(e){
+        switch(true){
+            case e.keyCode == 8: //backspace
+                context.command = context.command.substring(0,context.cursorIndex-1)+context.command.substring(context.cursorIndex,context.command.length);
+                context.cursorIndex>0?context.cursorIndex--:context.cursorIndex=0;
+                break; 
+            case e.keyCode == 13: //Enter
+                context.commandEntry(); 
+                break; 
+            case e.keyCode < 32: break;
+            case e.keyCode == 37: //Left Arrow
+                context.cursorIndex>0?context.cursorIndex--:context.cursorIndex=0;
+                break;
+            case e.keyCode == 38: //Up Arrow
+                context.prevEntIndex>0?context.prevEntIndex--:context.prevEntIndex=0; 
+                var i = context.prevEntIndex;
+                if(context.prevEntries.length > 0)
+                context.command = context.prevEntries[i]; 
+                context.cursorIndex = context.command.length;
+                break;
+            case e.keyCode == 39: //Right Arrow
+                context.cursorIndex<context.command.length?context.cursorIndex++:context.cursorIndex=context.command.length;
+                break;
+            case e.keyCode == 40: //Down Arrow
+                context.prevEntIndex<context.prevEntries.length?context.prevEntIndex++:context.prevEntIndex=context.prevEntries.length;
+                if(context.prevEntIndex == context.prevEntries.length) context.command = "";
+                else{
+                    var i = context.prevEntIndex;
+                    context.command = context.prevEntries[i];
+                }  
+                context.cursorIndex = context.command.length;
+                break;
+            case e.keyCode == 46: //Delete
+                context.command = context.command.substring(0,context.cursorIndex)+context.command.substring(context.cursorIndex+1,context.command.length);
+                break; 
+            default: //Other keys
+                var max_len = 153;
+                var key = (context.shifted && e.keyCode > 96 && e.keyCode < 123?toUpper(e.key):e.key);
+                if(context.command.length < max_len){
+                    context.command = context.command.substring(0,context.cursorIndex)+key+context.command.substring(context.cursorIndex,context.command.length);
+                    context.cursorIndex++;
+                }
+        }
+    },false);
 }
 
 Game.create = function(socket, canvasElement) {
@@ -36,15 +92,58 @@ Game.prototype.init = function(name, auth) {
     setInterval(function(){context.draw();}, 1000/FPS);
 }
 
+Game.prototype.commandEntry = function(){
+    //New entry
+    this.terminal.push("> "+this.command); 
+    if(this.command!="") this.prevEntries.push(this.command);
+    this.prevEntIndex = this.prevEntries.length;
+    this.cursorIndex = 0;
+
+    //Command validation
+    var valid = false;
+    for(var c in this.validCommands){
+        if(this.validCommands[c] == this.command){
+            //send command to server, unless...
+            if(this.command == "clear"){
+                this.terminal = [];
+                this.terminal.push("> "+this.command);
+            }
+            else{
+                //send command
+            }
+
+            valid = true;
+            break;
+        }
+    }
+
+    if(!valid){
+        this.terminal.push("Command not recognized as valid command.");
+        // this.terminal.push("The command '"+this.command+"' is not recognized as the name of a valid command, function, file or operable program. Check the spelling of the name or, if a path was included, verify the that the path is correct and try again.");
+        this.terminal.push("");
+    }
+
+    //Clear command line
+    this.command = "";
+}
+
 
 Game.prototype.draw = function() {
-
     //Resize the canvas
     this.drawing.resize();
 
     // Clear the canvas
     this.drawing.clear();
 
-    //Set scale
-    // this.drawing.setScale($("#gameArea").width());
+    // Draw background
+    this.drawing.background(this.status);
+
+    //Draw status
+    this.drawing.status(this.status);
+
+    //Draw terminal
+    this.drawing.terminal(this.status,this.terminal,this.command, this.cursorIndex);
+
+    //Draw chat
+    this.drawing.chat(this.status,[]);
 }
